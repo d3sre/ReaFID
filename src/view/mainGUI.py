2d51b60d09768c11010a068d1dc39f3a2fc8c81a
des <sachedes@students.zhaw.ch>
2014-08-22 22:57:55 +0200

    adding menu to window

--- src/view/mainGUI.py
+++ src/view/mainGUI.py
@@ -1,10 +1,40 @@
 #! /usr/bin/python
 
-import wx
 
-gameMain = wx.App(False)
+import tkinter as tk
 
-gameFrame = wx.Frame(None, wx.ID_ANY, "ReaFID")
 
-gameFrame.Show(True)
-gameMain.Main(Loop)
+class MainGui(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+        
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid()
+        
+    def menu(self):
+        self.subMenu = tk.Menu(self.menuRoot)
+        self.menuRoot.add_cascade(label="Menu", menu=self.subMenu)
+        self.subMenu.add_command(label="Card Configurationq", command=self.__aboutHandler)
+        
+    def topscore(self):
+        print "topscore"
+        
+    def about(self):
+        print "about"       
+        
+    def menuRoot(self):
+        menubar = tk.Menu() 
+        menubar.add_command(label="Menu", command=self.menu())
+        menubar.add_command(label="Topscore", command=self.topscore())
+        menubar.add_command(label="About", command=self.about())  
+        
+         
+        
+app = MainGui()
+app.master.title("ReaFID")
+app.mainloop()
+
+
