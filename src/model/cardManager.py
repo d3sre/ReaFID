d88148dc7916b494f00a021f02f9626fc7c6f376
des <sachedes@students.zhaw.ch>
2014-08-27 15:06:00 +0200

    moved singleton out of cardManager.py

--- src/model/cardManager.py
+++ src/model/cardManager.py
@@ -3,16 +3,17 @@
 import rFIDReader
 import cardFactory
 import pickle
+import singleton
 
 
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+# class Singleton(type):
+#     _instances = {}
+#     def __call__(cls, *args, **kwargs):
+#         if cls not in cls._instances:
+#             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+#         return cls._instances[cls]
 
-class CardManager(metaclass=Singleton):
+class CardManager(metaclass=singleton.Singleton):
     def __init__(self):
         self.activeCards = []
                  
